6edce48d808700e2dcd6d6ca3ccf50daeeb20044
Lee <lee.neufeld@ganjagurus.ca>
2022-01-10 15:14:30 -0700

    feat: added sign in

---
+++ src/components/sign-in/signin.component.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import CustomButton from "../custom-button/custom-button.component";
+import FormInput from "../form-input/form-input.components";
+import "./signin.styles.scss";
+
+class SignIn extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: "",
+      password: "",
+    };
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.setState({ email: "", password: "" });
+  };
+
+  handleChange = (event) => {
+    const { value, name } = event.target;
+    this.setState({ [name]: value });
+  };
+
+  render() {
+    return (
+      <div className="sign-in">
+        <h2>I already have an account</h2>
+        <span>Sign in with your email and password</span>
+
+        <form onSubmit={this.handleSubmit}>
+          <FormInput
+            name="email"
+            type="email"
+            handleChange={this.handleChange}
+            required
+            value={this.state.email}
+            label="email"
+          />
+
+          <FormInput
+            name="password"
+            type="password"
+            handleChange={this.handleChange}
+            required
+            label="password"
+            value={this.state.password}
+          />
+
+          <CustomButton type="submit">Sign In</CustomButton>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SignIn;
---
+++ src/pages/signin-and-signup/signin-and-signup.component.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import SignIn from "../../components/sign-in/signin.component";
+import "./signin-and-signup.styles.scss";
+
+const SignInAndSIgnUpPage = () => (
+  <div className="sign-inand-sign-up">
+    <SignIn />
+  </div>
+);
+
+export default SignInAndSIgnUpPage;
