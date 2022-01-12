7c1bb9e3d5bb2fc22f7fb7e92d7b073c91a95258
Lee <lee.neufeld@ganjagurus.ca>
2022-01-12 09:18:25 -0700

    added google sign in

--- src/pages/signin-and-signup/signin-and-signup.component.jsx
+++ src/pages/signin-and-signup/signin-and-signup.component.jsx
@@ -3,7 +3,7 @@ import SignIn from "../../components/sign-in/signin.component";
 import "./signin-and-signup.styles.scss";
 
 const SignInAndSIgnUpPage = () => (
-  <div className="sign-inand-sign-up">
+  <div className="sign-in-and-sign-up">
     <SignIn />
   </div>
 );
