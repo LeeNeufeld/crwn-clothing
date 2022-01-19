92ab5faf6b212851cab6782978933467323b6949
Lee <lee.neufeld@ganjagurus.ca>
2022-01-19 11:21:21 -0700

    feat: added sign up

--- src/pages/signin-and-signup/signin-and-signup.component.jsx
+++ src/pages/signin-and-signup/signin-and-signup.component.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import SignIn from "../../components/sign-in/signin.component";
+import SignUp from "../../components/sign-up/sign-up.component";
 import "./signin-and-signup.styles.scss";
 
 const SignInAndSIgnUpPage = () => (
   <div className="sign-in-and-sign-up">
     <SignIn />
+    <SignUp />
   </div>
 );
 
